4c507b7454bbc33ea360a5c35c94ac1e31913713
ylope1 <yoseflopez@gmail.com>
2025-12-15 06:06:14 -0300

    cambios 15/12/2025

---
+++ movimientos/ventas_cobros/arqueo_caja/index.php
@@ -0,0 +1,278 @@
+﻿<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>GUI ARQUEO DE CAJA</title>
+    <!-- Favicon-->
+    <link rel="icon" href="../../../favicon.ico" type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap Core Css -->
+    <link href="../../../plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Waves Effect Css -->
+    <link href="../../../plugins/node-waves/waves.css" rel="stylesheet" />
+
+    <!-- Animation Css -->
+    <link href="../../../plugins/animate-css/animate.css" rel="stylesheet" />
+
+    <!-- Bootstrap Material Datetime Picker Css -->
+    <link href="../../../plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css" rel="stylesheet" />
+
+    <!-- Sweetalert Css -->
+    <link href="../../../plugins/sweetalert/sweetalert.css" rel="stylesheet" />
+
+    <!-- JQuery DataTable Css -->
+    <link href="../../../plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- Bootstrap Select Css -->
+    <link href="../../../plugins/bootstrap-select/css/bootstrap-select.css" rel="stylesheet" />
+
+    <!-- Custom Css -->
+    <link href="../../../css/style.css" rel="stylesheet">
+
+    <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
+    <link href="../../../css/themes/all-themes.css" rel="stylesheet" />
+</head>
+
+<body class="theme-blue">
+
+    <?php require_once('../../../opciones.php'); ?>
+
+    <section class="content">
+        <div class="container-fluid">
+            
+            <div class="row clearfix">
+
+                <div class="col-md-12">
+                    
+                    <div class="card">
+                        <div class="header">
+                            <h2>Estado de Caja <small>CRUD de Arqueo de cajas</small> </h2>
+                        </div>
+                        <div class="body">
+                            <div class="row clearfix">
+                                <input type="hidden" value="0" id="txtOperacion"/>
+                                <input type="hidden" id="apertura_cierre_id" value="0">
+                                <input type="hidden" id="user_id" value="1">
+                                <input type="hidden" id="login" value="ylopez">
+                                <!-- CAMPO PARA CODIGO CON 3 COLUMNAS -->
+                                <div class="col-sm-3">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" id="caja_desc" class="form-control" disabled>
+                                        <label class="form-label">Caja</label>
+                                    </div>
+                                </div>
+                            </div>
+                                <div class="col-sm-3">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" id="estado_caja" class="form-control" disabled>
+                                        <label class="form-label">Estado</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-3">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" id="apertura_fec" class="form-control" disabled>
+                                        <label class="form-label">Fecha Apertura</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-3">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" id="cierre_fec" class="form-control" disabled>
+                                        <label class="form-label">Fecha Cierre</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- ================== APERTURA DE CAJA ================== -->
+        <div class="row clearfix" id="cardApertura">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="header">
+                        <h2>Apertura de Caja</h2>
+                    </div>
+                    <div class="body">
+                        <div class="row clearfix">
+                            <div class="col-sm-3">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" id="apertura_monto" class="form-control">
+                                        <label class="form-label">Monto de Apertura</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-3">
+                                <button type="button"
+                                        class="btn btn-success waves-effect"
+                                        onclick="abrirCaja();">
+                                    <i class="material-icons">lock_open</i>
+                                    ABRIR CAJA
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- ================== CIERRE DE CAJA ================== -->
+        <div class="row clearfix" id="cardCierre" style="display:none;">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="header">
+                        <h2>Cierre de Caja</h2>
+                    </div>
+                    <div class="body">
+                        <div class="row clearfix">
+
+                            <div class="col-sm-3">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" id="monto_sistema" class="form-control" disabled>
+                                        <label class="form-label">Monto Sistema</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-3">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" id="monto_arqueo" class="form-control">
+                                        <label class="form-label">Monto Arqueo</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-3">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" id="diferencia" class="form-control" disabled>
+                                        <label class="form-label">Diferencia</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-3">
+                                <button type="button"
+                                        class="btn btn-danger waves-effect"
+                                        onclick="cerrarCaja();">
+                                    <i class="material-icons">lock</i>
+                                    CERRAR CAJA
+                                </button>
+                            </div>
+
+                            <button type="button"
+                                    class="btn btn-warning waves-effect"
+                                    onclick="cancelarCierre();">
+                                <i class="material-icons">undo</i>
+                                CANCELAR
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- ================== LISTADO ================== -->
+        <div class="row clearfix">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="header">
+                        <h2>Registro de Aperturas y Cierres</h2>
+                    </div>
+                    <div class="body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-striped table-hover dataTable js-exportable">
+                                <thead>
+                                    <tr style="background-color:#e6e6e6;">
+                                        <th>ID</th>
+                                        <th>Caja</th>
+                                        <th>Apertura</th>
+                                        <th>Monto Apertura</th>
+                                        <th>Cierre</th>
+                                        <th>Monto Sistema</th>
+                                        <th>Monto Arqueo</th>
+                                        <th>Diferencia</th>
+                                        <th>Estado</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="tableBody">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Jquery Core Js -->
+    <script src="../../../plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="../../../plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="../../../plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+    <!-- Slimscroll Plugin Js -->
+    <script src="../../../plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="../../../plugins/node-waves/waves.js"></script>
+
+    <!-- SweetAlert Plugin Js -->
+    <script src="../../../plugins/sweetalert/sweetalert.min.js"></script>
+
+    <!-- Autosize Plugin Js -->
+    <script src="../../../plugins/autosize/autosize.js"></script>
+
+    <!-- Moment Plugin Js -->
+    <script src="../../../plugins/momentjs/moment.js"></script>
+
+    <!-- Bootstrap Material Datetime Picker Plugin Js -->
+    <script src="../../../plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js"></script>
+
+    <!-- Jquery DataTable Plugin Js -->
+    <script src="../../../plugins/jquery-datatable/jquery.dataTables.js"></script>
+    <script src="../../../plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+    <script src="../../../plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+    <script src="../../../plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+    <script src="../../../plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+    <script src="../../../plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+    <script src="../../../plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+    <script src="../../../plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+    <script src="../../../plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
+
+    <!-- Custom Js -->
+    <script src="../../../js/admin.js"></script>
+
+    <!-- Demo Js -->
+    <script src="../../../js/demo.js"></script>
+
+    <!-- Ruta Js (la url del backend o del api rest)-->
+    <script src="../../../js/ruta.js"></script>
+
+    <script src="metodos.js"></script>
+</body>
+
+</html>
